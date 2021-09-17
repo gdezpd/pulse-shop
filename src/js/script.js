cb23f9e8e8df7251db34f74e7a3fd6f8fa32ec65
Pavel <gdezpd@gmail.com>
2021-09-17 21:32:52 +0300

    add php

--- src/js/script.js
+++ src/js/script.js
@@ -99,6 +99,22 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name=phone]').mask("+375(99) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
 
 
